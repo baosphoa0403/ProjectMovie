c1c05015cfa2c753cd4e22c5600d2143287b3b88
bảo <baosphoa0403@gmail.com>
2020-04-21 21:14:38 +0700

    edit navbar

---
+++ index.js
@@ -0,0 +1,11 @@
+{/* <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <!-- <div class="container"> -->
+      <div class="col-md-2  col-sm-2 col-xs-2">
+        <div class="logo">
+          <a href=""><img src="./img/logo2.png" alt=""></a>
+        </div>
+      </div>
+     
+      <!-- </div> -->
+    
+    </nav> */}
